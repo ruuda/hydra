9d888def22bb332ec0f5c7958b963ab145336a02
Ruud van Asseldonk <dev@veniogames.com>
2016-08-17 22:32:23 +0200

    Fix encryption and decryption js interop
    The key was to curry the functions on the Javascript side.

--- src/Native/Sjcl.js
+++ src/Native/Sjcl.js
@@ -1,12 +1,16 @@
 // Elm mangles names based on username and module name. So 'Native.Sjcl' in the
 // repository 'ruuda/hydra' gets the ugly name below.
 var _ruuda$hydra$Native_Sjcl = {
-  'encrypt': {
-    'arity': 2,
-    // The 'sjcl' object is provided by SJCL itself in sjcl.js.
-    'func': sjcl.encrypt
+  // Functions in Elm are curried, so curry the SJCL functions here.
+  'encrypt': function (key) {
+    return function (plaintext) {
+      // The 'sjcl' object is provided by SJCL itself in sjcl.js.
+      return sjcl.encrypt(key, plaintext);
+    };
   },
-  // For reasons unknown to me, 'encrypt' must be a wrapper object as above,
-  // but 'decrypt' is called directly, and it must be a function.
-  'decrypt': sjcl.decrypt
+  'decrypt': function (key) {
+    return function (ciphertext) {
+      return sjcl.decrypt(key, ciphertext);
+    };
+  }
 };
